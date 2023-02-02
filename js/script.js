f2754d835046849c1bae561cf59686e4e6543d61
JimGardner0204 <jpginvoicing@outlook.com>
2023-02-02 09:58:55 +0000

    js input for question function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,59 @@
-console.log("hello");
+const quizContainer = document.getElementById('quizquestions');
+const resultsContainer = document.getElementById('results')
+const submitButton - document.getElementById('submit');
+
+
+function constructQuiz() {
+
+    const output = [];
+
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+            const answers = [];
+
+            for(letter in currentQuestion.answers){
+
+                //add a html radio button
+                answers.push(
+                    <label>
+                        <input type ="radio" name ="question${questionNumber}" value="${letter}">
+                            ${letter} :
+                            ${currentQuestion.answers[letter]}
+                    </label>
+                );
+            }
+
+            output.push(
+                <div class="question"> ${currentQuestion.question}</div>
+            )
+
+        }
+    )
+
+
+
+}
+
+function displayResults() {}
+
+//display quiz immediately to user
+constructQuiz();
+
+//display results to user upon submitting
+submitButton.addEventListener('click', showResults);
+
+
+const myQuestions = [
+    {
+        question: "My name is what?",
+        answers: {
+            a: "James",
+            b: "John",
+            c: "Deb"
+
+        },
+        correctAnswer : "a"
+    },
+    //next question and so on....
+]
