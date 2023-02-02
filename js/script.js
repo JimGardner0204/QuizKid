02b3ec8ddd93a28600f4c93e4e02f179439e390e
JimGardner0204 <jpginvoicing@outlook.com>
2023-02-02 11:22:13 +0000

    function to show questions and show results created

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 function createQuiz(questions, quizContainer, resultsContainer, submitButton) {
 
     function showQuestions(questions, quizContainer) {
-        //await code...
+        let output = [];
+
+        let answers;
+
+        for(let i=0; i<questions.length; i++){
+            answers = [];
+
+            for(letter in questions[i].answers){
+
+                answers.push(
+                    `<label>'
+                    + <input type = "radio" name="question" value="${letter}">`
+                    + letter + ': '
+                    + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+
+            output.push(
+                '<div class="questions">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        quizContainer.innerHTML = output.join('');
     }
 
     function showResults(questions, quizContainer, resultsContainer) {
@@ -14,3 +38,28 @@ function createQuiz(questions, quizContainer, resultsContainer, submitButton) {
         showResults(questions, quizContainer, resultsContainer);
     }
 }
+
+let myQuestions = [
+    {
+        question: "What is the name of my dog?",
+
+        answers: {
+            a: 'Jim',
+            b: 'John',
+            c: 'Sid'
+        },
+
+        correctAnswer: 'a'
+    },
+    {
+        question: "What is the name of my cat?",
+
+        answers: {
+            a: 'Ben',
+            b: 'Stella',
+            c: 'Sid'
+        },
+
+        correctAnswer: 'c'
+    }
+];
