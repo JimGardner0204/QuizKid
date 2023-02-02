98e9b044a7827e610fe75105aebc103ed61b3f61
JimGardner0204 <jpginvoicing@outlook.com>
2023-02-02 10:44:38 +0000

    different functions added for show questions and show results

--- js/script.js
+++ js/script.js
@@ -1,62 +1,16 @@
-const quizContainer = document.getElementById('quizquestions');
-const resultsContainer = document.getElementById('results')
-const submitButton - document.getElementById('submit');
+function createQuiz(questions, quizContainer, resultsContainer, submitButton) {
 
+    function showQuestions(questions, quizContainer) {
+        //await code...
+    }
 
-function constructQuiz() {
+    function showResults(questions, quizContainer, resultsContainer) {
+        //await code..
+    }
 
-    const output = [];
+    showQuestions(questions, quizContainer);
 
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-
-            const answers = [];
-
-            for(letter in currentQuestion.answers){
-
-                //add a html radio button
-                answers.push(
-                    <label>
-                        <input type ="radio" name ="question${questionNumber}" value="${letter}">
-                            ${letter} :
-                            ${currentQuestion.answers[letter]}
-                    </label>
-                );
-            }
-
-            output.push(
-                <div class="question"> ${currentQuestion.question}</div>
-                <div class="answers"> ${answers.join('')} </div>
-            );
-
-        }
-    );
-
-    quizContainer.innerHTML = output.join('');
-
-
-
+    submitButton.onClick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    }
 }
-
-function displayResults() {}
-
-//display quiz immediately to user
-constructQuiz();
-
-//display results to user upon submitting
-submitButton.addEventListener('click', showResults);
-
-
-const myQuestions = [
-    {
-        question: "My name is what?",
-        answers: {
-            a: "James",
-            b: "John",
-            c: "Deb"
-
-        },
-        correctAnswer : "a"
-    },
-    //next question and so on....
-]
